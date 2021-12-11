53ecfd3205b78e437153a96556aa2d321b6df891
hub33k <hub33k@yahoo.com>
2021-12-11 16:33:05 +0100

    Add relative paths to 3-react (vite)

--- 3-react/vite.config.ts
+++ 3-react/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3-react/',
+  base: './',
   build: {
     outDir: '../public/3-react',
   },
